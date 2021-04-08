740cfddb4daaf1103359a5d878692632ed4e6d84
williamvlz <williamvlz@hotmail.com>
2021-04-08 08:19:56 -0500

    PARCIAL

---
+++ TALLERES/taller5.py
@@ -0,0 +1,14 @@
+#....................taller 5.................#
+listaEdades = [12, 15, 16, 19, 13, 24, 11, 18, 33, 29]
+
+print ("#"*20, "edad" ,"#"*20)
+
+#....................PUNTO 1.................#
+def lista ():
+    for elemento in listaEdades
+    print elemento
+
+lista()
+#..................PUNTO 2.................#
+def Edades ():
+    print ("ESTE ES EL MAYOR DE LA LISTA")
