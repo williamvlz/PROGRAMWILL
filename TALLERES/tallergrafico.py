e087f5fed9778761efeff3e816b4990b208d139d
williamvlz <williamvlz@hotmail.com>
2021-05-19 15:09:07 -0500

    actial

---
+++ TALLERES/tallergrafico.py
@@ -0,0 +1,11 @@
+#...................taller 8..............#
+
+import matplotib.pylot as plt
+mes = ["enero", "febrero", "marzo", "abril", "mayo", "junio"]
+ingresos = [100, 200, 300, 400, 500, 600]
+plt.bar (mes, ingresos, width = 0.8, color = "r")
+plt.title ("Cantidad de ingresos")
+plt.xlabel ("mes")
+plt.ylabel("ingresos")
+plt.savefig("GraficoBarra.png")
+plt.show()
